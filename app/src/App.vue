4a82331913f0d65b5197fd6b6de88c864cf035b8
tk1351 <takakikitsui0111@gmail.com>
2022-01-19 21:28:24 +0900

    feat: stepperを表示するコンポーネント作成

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
+import Stepper from './components/Stepper.vue'
 </script>
 
 <template>
+  <Stepper />
 </template>
 
 <style>
 #app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   height: 100vh;
   width: 100vw;
---
+++ app/src/components/Stepper.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="stepper"></div>
+</template>
+
+<style scoped>
+.stepper {
+  width: 700px;
+  height: 700px;
+  border: 1px solid #fff;
+}
+</style>
